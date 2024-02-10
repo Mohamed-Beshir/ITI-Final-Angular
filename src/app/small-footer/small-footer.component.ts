0410f2be8cd6ffaca39fd9fa9f3ef8f57d49e7f2
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-02-10 14:05:31 +0200

    Small Footer Component has been added

---
+++ src/app/small-footer/small-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-footer.component.html',
+  styleUrl: './small-footer.component.css'
+})
+export class SmallFooterComponent {
+
+}
