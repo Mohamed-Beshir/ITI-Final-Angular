00a632da2589b33479ba9055dd4c4531f9a64c3a
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-02-10 14:06:19 +0200

    Big Footer Component has been added

---
+++ src/app/big-footer/big-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-big-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './big-footer.component.html',
+  styleUrl: './big-footer.component.css'
+})
+export class BigFooterComponent {
+
+}
