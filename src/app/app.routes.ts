d1c741f255741df575c56aa1fec9a25077cae0d5
Abdelrahman Ahmed <abdelrahman.elazizy.1999@gmail.com>
2024-02-15 18:03:42 +0200

    Add provideHttpClient to app.config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
-
-export const routes: Routes = [
- { 'path':'register' , component:RegisterComponent }
-=======
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-
-
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'signin', component: SignInComponent },
->>>>>>> d96eb3889a0de516934cf534a330d0edd95fde3f
+    { path:'register' , component:RegisterComponent }
 ];
